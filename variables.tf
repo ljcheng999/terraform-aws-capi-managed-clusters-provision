2f79764c2847d73282162c37f340da1f3227bdae
ljcheng-tech <johnny.cheng919@gmail.com>
2025-06-25 17:43:52 -0600

    cleanup

--- variables.tf
+++ variables.tf
@@ -7,14 +7,6 @@ variable "tags" {
   default = {}
 }
 
-
-# variable "federated_arns_assume_role_from_dm_core" {
-#   default = []
-# }
-# variable "federated_statements_allow_dm_core_capi_clusters_for_irsa" {
-#   default = []
-# }
-
 variable "aws_account_id_upstream" {
   type    = string
   default = ""
