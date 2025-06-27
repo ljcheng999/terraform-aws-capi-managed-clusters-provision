21d7c07688fff7fce6af4349631f33713f1f05af
ljcheng-tech <johnny.cheng919@gmail.com>
2025-06-27 17:54:49 -0600

    add if condition for policies

--- capa-nodegroup-iam-policies.tf
+++ capa-nodegroup-iam-policies.tf
@@ -1,5 +1,6 @@
 
-resource "aws_iam_policy" "amazon_eks_ingress_alb_policy" {
+resource "aws_iam_policy" "amazon_eks_ingress_elb_policy" {
+  count       = var.aws_account_id_allow_assume_self != var.aws_account_id_upstream ? 1 : 0
   name        = var.capa_ingress_elb_policy_name
   description = "Policy for CAPI Cluster ELB policy"
   policy      = file("${path.module}/templates/aws/capa-elb-iam-policy.json")
@@ -12,6 +13,7 @@ resource "aws_iam_policy" "amazon_eks_ingress_alb_policy" {
 }
 
 resource "aws_iam_policy" "capa_nodes_assume_role_policy" {
+  count       = var.aws_account_id_allow_assume_self != var.aws_account_id_upstream ? 1 : 0
   name        = var.capa_nodes_assume_policy
   description = "Policy for CAPA node assume policy"
   policy      = file("${path.module}/templates/aws/capa-nodes-assume-role-policy.json")
@@ -24,6 +26,7 @@ resource "aws_iam_policy" "capa_nodes_assume_role_policy" {
 }
 
 resource "aws_iam_policy" "capa_nodes_karpender_controller_policy" {
+  count       = var.aws_account_id_allow_assume_self != var.aws_account_id_upstream ? 1 : 0
   name        = var.capa_nodes_karpenter_controller_policy_name
   description = "Policy for CAPA node karpender controller policy"
   policy      = file("${path.module}/templates/aws/capa-nodes-karpender-controller-policy.json")
